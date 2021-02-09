7c80b5809ada17672a0bc3174cdc1f8ef92ecddb
Punk_UnDeaD <punkundead@gmail.com>
2021-02-09 11:34:25 +0300

    stub ServiceEntityRepository

--- src/Model/Media/Entity/Repository/ImageRepository.php
+++ src/Model/Media/Entity/Repository/ImageRepository.php
@@ -4,32 +4,23 @@ declare(strict_types=1);
 
 namespace App\Model\Media\Entity\Repository;
 
-use App\Model\GetOneRepositoryInterface;
 use App\Model\Media\Entity\Image;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityNotFoundException;
 use Doctrine\Persistence\ManagerRegistry;
 
-class ImageRepository extends ServiceEntityRepository implements GetOneRepositoryInterface
+/**
+ * @extends ServiceEntityRepository<Image>
+ */
+class ImageRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Image::class);
     }
 
-    /**
-     * @psalm-suppress MoreSpecificImplementedParamType
-     *
-     * @param string $id
-     */
-    public function get($id): Image
+    public function get(string $id): Image
     {
-        /** @var ?Image $image */
-        $image = $this->find($id);
-        if (!$image) {
-            throw EntityNotFoundException::fromClassNameAndIdentifier('Image', [$id]);
-        }
-
-        return $image;
+        return $this->find($id) ?? throw EntityNotFoundException::fromClassNameAndIdentifier('Image', [$id]);
     }
 }
